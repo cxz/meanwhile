17bc1eb517b1c70f0b37147989c636ebaabc1084
taliesein <taliesein>
2004-06-28 07:21:15 +0000

    new release. yay

--- web/release.php
+++ web/release.php
@@ -2,9 +2,10 @@
 	global $mw_release, $mw_release_url;
 	global $mw_gaim, $mw_gaim_url;
 
-	$mw_release = "meanwhile 0.2";
-	$mw_release_url = "http://sf.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=242750";
+	$mw_release = "meanwhile 0.3";
+	$mw_release_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=249199";
 
-	$mw_gaim = "meanwhile-gaim 0.78";
-	$mw_gaim_url = "http://sf.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=242752";
+	$mw_gaim = "meanwhile-gaim 0.79";
+	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=249201";
+
 ?>
