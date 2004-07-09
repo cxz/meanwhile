fed60b58b88c174b7ae56f5f2ae2314df2423e45
taliesein <taliesein>
2004-07-09 00:27:14 +0000

    words

--- web/download/main.php
+++ web/download/main.php
@@ -11,8 +11,8 @@ global $mw_gaim, $mw_gaim_url;
 
 
 <h2>CVS</h2>
-<p>The CVS versions of Meanwhile are built against the (near) current CVS
-version of Gaim. Since the Meanwhile gaim plugin code is therefore aiming at a
+<p>The CVS versions of meanwhile-gaim are built against the (near) current CVS
+version of Gaim. Since the meanwhile-gaim plugin code is therefore aiming at a
 moving target, ocasionally the build will break. Ideally however, changes to
 the Gaim prpl or plugin interfaces will be reflected in Meanwhile within a day
 or two. If you're still feeling brave, simply follow these steps:</p>
@@ -22,22 +22,10 @@ or two. If you're still feeling brave, simply follow these steps:</p>
 <li>Checkout the <code>gaim</code> module from Sourceforge anonymous CVS<br />
 <a href="http://sourceforge.net/cvs/?group_id=235">http://sourceforge.net/cvs/?group_id=235</a></li>
 
-<li>Checkout the <code>meanwhile</code>, and
-<code>meanwhile-gaim</code> modules from Sourceforge anonymous CVS</li>
+<li>Build and install first gaim, then meanwhile, then meanwhile-gaim. Simply running the following in each sandbox should work:<br />
+<code>./autogen.sh &amp;&amp; ./configure &amp;&amp; make all &amp;&amp; su -c "make install"</code></li>
 
-<li>Build and install Gaim</li>
-
-<li>Set the <code>gaim_src_dir</code> environment variable to point to the <code>src</code> directory in your Gaim sandbox</li>
-
-<li>Run <code>./autogen.sh &amp;&amp; ./configure --with-gaim-source=$gaim_src_dir</code></li>
-
-<li>Run <code>make all</code></li>
-
-<li>Run <code>make install &amp;&amp; ldconfig</code> as root. You may need to
-set the <code>libdir</code>, <code>includedir</code> and <code>datadir</code>
-variables to match you preferred installation points. Defaults for CVS point to
-<code>/usr/local</code>, while the packaged installs point to <code>/usr</code>.
-You'll need to match these to the Gaim installation.</li>
+<li>Run <code>ldconfig</code> as root. This will ensure that your system will be able to load the meanwhile libraries when gaim starts.</li>
 
 <li>Start up Gaim, and with luck you'll be ready to go!</li>
 </ol>
