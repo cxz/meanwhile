642ffad9fbfb3e01655469ebaaa68d7644b84831
taliesein <taliesein>
2004-08-31 09:59:58 +0000

    *** empty log message ***

--- web/release.php
+++ web/release.php
@@ -2,10 +2,12 @@
 	global $mw_release, $mw_release_url;
 	global $mw_gaim, $mw_gaim_url;
 
-	$mw_release = "meanwhile 0.3";
+	$mw_release = "meanwhile 0.3-1";
 	$mw_release_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=249199";
 
-	$mw_gaim = "gaim-meanwhile 0.80";
-	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=253394";
+	$mw_gaim = "gaim-meanwhile 0.82";
+	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=263608";
+
+	
 
 ?>
