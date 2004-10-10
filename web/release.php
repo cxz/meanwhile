90b43564640aa557b77d56c95ae17d322ed6d4f8
taliesein <taliesein>
2004-10-10 05:15:34 +0000

    keeping things mildly up to date

--- web/release.php
+++ web/release.php
@@ -5,8 +5,8 @@
 	$mw_release = "meanwhile 0.3-1";
 	$mw_release_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=249199";
 
-	$mw_gaim = "gaim-meanwhile 0.82";
-	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=263608";
+	$mw_gaim = "gaim-meanwhile 1.0.0";
+	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=268847";
 
 	
 
