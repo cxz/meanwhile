c7c671f317b5868f1322c8794315c481485d85f4
taliesein <taliesein>
2004-11-10 05:11:19 +0000

    new release

--- web/release.php
+++ web/release.php
@@ -5,8 +5,8 @@
 	$mw_release = "meanwhile 0.3-1";
 	$mw_release_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=249199";
 
-	$mw_gaim = "gaim-meanwhile 1.0.1";
-	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=275526";
+	$mw_gaim = "gaim-meanwhile 1.0.2";
+	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=281535";
 
 	
 
