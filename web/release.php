e1a57a2a0e8856c50353ff59668772446d437661
taliesein <taliesein>
2004-11-29 23:40:21 +0000

    easier to manage, and valid xhtml 1.0 transitional

--- web/release.php
+++ web/release.php
@@ -2,12 +2,19 @@
 	global $mw_release, $mw_release_url;
 	global $mw_gaim, $mw_gaim_url;
 
+	// the files base for our project
+	$base_url = "http://sourceforge.net/project/showfiles.php" .
+		"?group_id=110565";
+
+	// the only part that should need to change for these is the
+	// name of the release, and the release_id
+
 	$mw_release = "meanwhile 0.3-1";
-	$mw_release_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119439&release_id=249199";
+	$mw_release_url = $base_url . "&amp;package_id=119439" .
+		"&amp;release_id=249199";
 
 	$mw_gaim = "gaim-meanwhile 1.0.2";
-	$mw_gaim_url = "http://sourceforge.net/project/showfiles.php?group_id=110565&package_id=119703&release_id=281535";
-
-	
-
+	$mw_gaim_url =  $base_url . "&amp;package_id=119703" .
+		"&amp;release_id=281535";
 ?>
+
