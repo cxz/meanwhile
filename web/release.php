0cbe6bf714b6e71aef45e0c0fa6de544616cdc29
taliesein <taliesein>
2005-05-02 03:19:58 +0000

    new releases

--- web/release.php
+++ web/release.php
@@ -9,12 +9,12 @@
 	// the only part that should need to change for these is the
 	// name of the release, and the release_id
 
-	$mw_release = "meanwhile 0.4.0";
+	$mw_release = "meanwhile 0.4.1";
 	$mw_release_url = $base_url . "&amp;package_id=119439" .
-		"&amp;release_id=253395";
+		"&amp;release_id=324557";
 
-	$mw_gaim = "gaim-meanwhile 1.2.0";
+	$mw_gaim = "gaim-meanwhile 1.2.1";
 	$mw_gaim_url =  $base_url . "&amp;package_id=119703" .
-		"&amp;release_id=313801";
+		"&amp;release_id=324550";
 ?>
 
