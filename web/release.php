f08c2915bb21c24c1b57fc2406b546417b407093
taliesein <taliesein>
2005-12-30 02:44:32 +0000

    releases are fun

--- web/release.php
+++ web/release.php
@@ -9,12 +9,12 @@
 	// the only part that should need to change for these is the
 	// name of the release, and the release_id
 
-	$mw_release = "meanwhile 1.0.1";
+	$mw_release = "meanwhile 1.0.2";
 	$mw_release_url = $base_url . "&amp;package_id=119439" .
-		"&amp;release_id=378683";
+		"&amp;release_id=356768";
 
-	$mw_gaim = "gaim-meanwhile 1.2.6";
+	$mw_gaim = "gaim-meanwhile 1.2.7";
 	$mw_gaim_url =  $base_url . "&amp;package_id=119703" .
-		"&amp;release_id=377869";
+		"&amp;release_id=381391";
 ?>
 
